76ffec697862cbdd1ab05b97b6f97029b987bce6
Pat Sluth <pat.sluth@gmail.com>
2019-01-28 17:45:13 -0700

    a

--- Sluthware/Classes/Foundation/Collection+KeyPath.swift
+++ Sluthware/Classes/Foundation/Collection+KeyPath.swift
@@ -26,7 +26,8 @@ public extension Collection
 		})
 	}
 	
-	public func sorted<Value>(by keyPath: KeyPath<Element, Value>, _ comparator: (Value, Value) -> Bool) -> [Element]
+	public func sorted<Value>(by keyPath: KeyPath<Element, Value>,
+							  _ comparator: (Value, Value) -> Bool) -> [Element]
 		where Value: Comparable
 	{
 		return self.sorted(by: {
@@ -39,6 +40,40 @@ public extension Collection
 	//		self = self.sorted(by: keyPath, comparator)
 	//	}
 }
+
+
+
+// TODO: Move somewhere else
+public extension Dictionary
+	where Key: Comparable
+{
+	public func sorted(by keyComparator: (Key, Key) -> Bool,
+					   _ valueTransformer: (Value) -> Value) -> [(key: Key, value: Value)]
+	{
+		return self.sorted(by: {
+			keyComparator($0.key, $1.key)
+		}).map({
+			(key: $0.key, value: valueTransformer($0.value))
+		})
+		
+		//		return self.reduce(into: [(key: Key, value: Value)](), {
+		////			let key = $1[keyPath: keyPath]
+		////			var elements = $0[key, default: []]
+		////			elements.append($1)
+		////			$0[key] = elements
+		//		})
+	}
+}
+
+//public extension Dictionary
+//	where Key: Comparable,
+//	Value: AnyCollection
+//{
+//	public func sorted5(by comparator: (Key, Key) -> Bool) -> [(key: Key, value: Value)]
+//	{
+//		return []
+//	}
+//}
 
 
 
