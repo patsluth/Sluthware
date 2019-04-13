bcff84d04bbc6bf5891a75a5413ea38e67c721a8
Pat Sluth <pat.sluth@gmail.com>
2019-04-13 10:49:52 -0600

    a

--- Sluthware/Classes/UIKit/UIView.swift
+++ Sluthware/Classes/UIKit/UIView.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import UIKit
 
 
 
@@ -19,6 +20,46 @@ public extension NSObjectProtocol
 	func superview(_ superview: UIView) -> Self
 	{
 		superview.addSubview(self)
+		
+		return self
+	}
+	
+	
+	
+	/// wrapper for systemLayoutSizeFitting(targetSize:)
+	func systemSize(fitting targetSize: CGSize = UIView.layoutFittingCompressedSize) -> CGSize
+	{
+		return self.systemLayoutSizeFitting(targetSize)
+	}
+	
+	/// wrapper for systemLayoutSizeFitting(targetSize:withHorizontalFittingPriority:verticalFittingPriority)
+	func systemSize(fitting targetSize: CGSize = UIView.layoutFittingCompressedSize,
+					horizontal: UILayoutPriority,
+					vertical: UILayoutPriority) -> CGSize
+	{
+		return self.systemLayoutSizeFitting(targetSize,
+											withHorizontalFittingPriority: horizontal,
+											verticalFittingPriority: vertical)
+	}
+	
+	/// wrapper for setContentHuggingPriority(priority:for:)
+	/// The priority with which a view resists being made larger than its intrinsic size.
+	@discardableResult
+	func contentHugging(priority: UILayoutPriority,
+						for axis: NSLayoutConstraint.Axis) -> Self
+	{
+		self.setContentHuggingPriority(priority, for: axis)
+		
+		return self
+	}
+	
+	/// wrapper for setContentCompressionResistancePriority(priority:for:)
+	/// The priority with which a view resists being made smaller than its intrinsic size.
+	@discardableResult
+	func compressionResistance(priority: UILayoutPriority,
+							   for axis: NSLayoutConstraint.Axis) -> Self
+	{
+		self.setContentCompressionResistancePriority(priority, for: axis)
 		
 		return self
 	}
