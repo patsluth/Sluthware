e421a63134f66aa08d3bb38e5d0e41b3481bb6e9
Pat Sluth <pat.sluth@gmail.com>
2019-04-14 11:41:11 -0600

    a

--- Sluthware/Classes/Alamofire/Alamofire+SW.swift
+++ Sluthware/Classes/Alamofire/Alamofire+SW.swift
@@ -13,6 +13,42 @@ import Alamofire
 
 
 
+/// Alamofire extension that attempts to decode response
+public extension DataRequest
+{
+	@discardableResult
+	func response<T>(type: T.Type,
+					 queue: DispatchQueue? = nil,
+					 options: JSONSerialization.ReadingOptions = .allowFragments,
+					 completionHandler: @escaping (DataResponse<T>) -> Void) -> Self
+		where T: Decodable
+	{
+		return self.responseJSON(completionHandler: { response in
+			switch response.result {
+			case .success(let json):
+				completionHandler(DataResponse<T>(request: response.request,
+												  response: response.response,
+												  data: response.data,
+												  result: Result(json)))
+			case .failure(let error):
+				completionHandler(DataResponse<T>(request: response.request,
+												  response: response.response,
+												  data: response.data,
+												  result: Result.failure(error)))
+			}
+		})
+		//		return response(
+		//			queue: queue,
+		//			responseSerializer: DataRequest.jsonResponseSerializer(options: options),
+		//			completionHandler: completionHandler
+		//		)
+	}
+}
+
+
+
+
+
 public extension Alamofire.Result
 	where Value: Decodable
 {
